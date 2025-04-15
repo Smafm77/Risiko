ca414273b98f04f70583b79766f934c178a06649
Majbritt Siebenthaler <msiebenthaler@stud.hs-bremen.de>
2025-04-15 16:13:55 +0200

    Vorschläge für Spieler-Methoden

--- Risiko/src/Spieler.java
+++ Risiko/src/Spieler.java
@@ -3,8 +3,14 @@ public class Spieler {
     public String farbe;
     public int einheiten;
     public boolean alive;
+    //List<Land> terretory; Liste aller besetzten Länder
 
     public void sterben(){
         this.alive = false;
     }
+
+    // void recieveTroops(){ermittelt zum Zuganfang zustehende Truppen; assignTroops();}
+    // void assignTroops(int troops){Füge 'troops' von Spieler ausgewählten Ländern zu}
+    // void moveTroops(int troops, Land herkunft, Land ziel){Prüfe ob Zug möglich; Herkunft - troops; ziel + troops}
+    // int getTroops (){int sum = 0; for (Land land : terretory){sum += land.troops} return sum;}
 }
