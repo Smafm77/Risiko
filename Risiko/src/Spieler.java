130a87472cffa0d27e5fa01e9bfc5156526807f6
HannahGie <hgienk@stud.hs-bremen.de>
2025-04-15 15:29:20 +0200

    Klasse Einheiten gelöscht, boolean alive und methode sterben in Spieler übertragen

--- Risiko/src/Spieler.java
+++ Risiko/src/Spieler.java
@@ -2,4 +2,9 @@ public class Spieler {
     public String name;
     public String farbe;
     public int einheiten;
+    public boolean alive;
+
+    public void sterben(){
+        this.alive = false;
+    }
 }
