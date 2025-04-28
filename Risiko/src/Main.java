08e2323a1a1a94385fa480cdc5be91dcc244e779
HannahGie <hgienk@stud.hs-bremen.de>
2025-04-28 15:02:39 +0200

    Anfang fuer CUI

--- Risiko/src/Main.java
+++ Risiko/src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.util.Scanner;
 
+public class Main {
+    public static void starteSpiel() {
+        System.out.println("Dummy damit das Programm nicht abstürzt");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        System.out.println("Risiko");
+
+        while (running) {
+            System.out.println("1. Spiel starten");
+            System.out.println("2. Beenden");
+            System.out.println("Bitte eine Option auswählen");
+            String auswahl = scanner.nextLine();
+
+            switch (auswahl) {
+                case "1":
+                    starteSpiel();
+                    break;
+                case "2":
+                    System.out.println("Wird beendet");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Ungültige Eingabe");
+
+            }
+        }
     }
 }
