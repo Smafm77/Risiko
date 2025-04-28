fda1160a49be0d6875520b659c8cb574770e34c2
HannahGie <hgienk@stud.hs-bremen.de>
2025-04-28 15:16:55 +0200

    Dummy aus starteSpiel gegen Spieler Konstrukteur ausgetauscht

--- Risiko/src/Main.java
+++ Risiko/src/Main.java
@@ -1,15 +1,20 @@
-import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
     public static void starteSpiel() {
-        System.out.println("Dummy damit das Programm nicht abstürzt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte die Anzahl an Spielern eingeben:");
+        int anzahlSpieler = scanner.nextInt();
+        scanner.nextLine(); //Weil scanner.nextInt immer mucken macht einfach nochmal nextLine "einlesen"
+        for(int i = 1; i<= anzahlSpieler; i++){
+            Spieler spieler = new Spieler();
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
-        System.out.println("Risiko");
+        System.out.println("Willkommen zu Risiko");
 
         while (running) {
             System.out.println("1. Spiel starten");
