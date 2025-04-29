c30c7227ec514cc2a2db6ca5e1ba2361bf6adf7e
Majbritt Siebenthaler <msiebenthaler@stud.hs-bremen.de>
2025-04-29 15:57:50 +0200

    added Kontinent.checkBesitzer()

--- Risiko/src/Kontinent.java
+++ Risiko/src/Kontinent.java
@@ -1,7 +1,24 @@
 public class Kontinent {
-    public Land gebiete[];
+    public Land[] gebiete;
     public int buff;
-    public Spieler einzigerBesitzer;
+    public Spieler einzigerBesitzer; //Kann null sein, falls Kontinent mehrere Besatzer hat
 
     //Todo Länder den Kontinenten zuweisen
+
+    public void checkBesitzer(){
+        boolean isSame = true;
+        for (int i = 1; i < gebiete.length; i++){
+            if (!gebiete[i].besitzer.equals(gebiete[0].besitzer)) {
+                isSame = false;
+                break;
+            }
+        }
+        if (isSame){
+            einzigerBesitzer = gebiete[0].besitzer;
+        } else {
+            einzigerBesitzer = null;
+        }
+    }
+    //ToDO Update Kontinentbesitzer bei jedem Besitzwechsel von zugehörigem Land
+    // -->boolenrückgabe bei Änderung möglich einzufügen
 }
