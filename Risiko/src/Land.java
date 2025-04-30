e33298fae8c71d39e973d8fbb9b68d0c0b14b7d8
Majbritt Siebenthaler <msiebenthaler@stud.hs-bremen.de>
2025-04-30 22:30:43 +0200

    Verbindungen zum Truppenziehen prüfbar gemacht (directNeighbors, connectionOptions, connectionPossible) angabe angrenzender Feinde eines Landes

--- Risiko/src/Land.java
+++ Risiko/src/Land.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class Land {
     public String name;
@@ -13,6 +14,29 @@ public class Land {
         this.strength = strength;
         this.name = name;
         this.nachbarn.addAll(Arrays.asList(nachbarn));
+    }
+
+    //ToDo Teste ob connectionOptions & directNeighbors funktioniert, sobald Karte feststeht (sorge um Beibehalten von Veränderungen der Sets)
+    public boolean connectionPossible(Land ziel){
+        return connectionOptions().contains(ziel);
+    }
+    public HashSet<Land> connectionOptions(){
+        HashSet<Land> neighborhood = new HashSet<>();
+        HashSet<Land> barbarians = new HashSet<>();
+        directNeighbors(this, neighborhood, barbarians);
+        return neighborhood;
+    }
+    private void directNeighbors(Land currentStation, HashSet<Land> neighborhood, HashSet<Land> barbarians){
+        for (Land land : currentStation.nachbarn){
+            if (!neighborhood.contains(land) && !barbarians.contains(land)){
+                if (land.besitzer == currentStation.besitzer){
+                    neighborhood.add(land);
+                    directNeighbors(land, neighborhood, barbarians);
+                } else {
+                    barbarians.add(land);
+                }
+            }
+        }
     }
 
     //region Getters and Setters
@@ -31,6 +55,9 @@ public class Land {
     }
     public void addNachbar(Land nachbar){
         nachbarn.add(nachbar);
+    }
+    public HashSet<Land> getFeindlicheNachbarn(){//Todo Test this
+        return (HashSet<Land>) nachbarn.stream().filter(l -> l.besitzer != this.besitzer).collect(Collectors.toSet());
     }
 
     //Wir brauchen wahrscheinlich erstmal unsere Map damit wir wissen welches Land welchen Nachbarn hat.
