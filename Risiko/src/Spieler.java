068c5c244ff4be01da7e4a914783762b93dcc94e
HannahGie <hgienk@stud.hs-bremen.de>
2025-05-11 17:21:00 +0200

    neueArmee Rechnung geprüft und passende gewählt und unpassende gelöscht

--- Risiko/src/Spieler.java
+++ Risiko/src/Spieler.java
@@ -18,25 +18,32 @@ public class Spieler {
         this.id = id;
         this.einheiten = 0;
     }
+
     //endregion
     public String getName() {
         return name;
     }
+
     public int getId() {
         return id;
     }
+
     public String getFarbe() {
         return farbe;
     }
+
     public int getEinheiten() {
         return einheiten;
     }
+
     public boolean isAlive() {
         return alive;
     }
+
     public ArrayList<Land> getBesetzteLaender() {
         return besetzteLaender;
     }
+
     public HashSet<Karte> getKarten() {
         return karten;
     }
@@ -46,7 +53,7 @@ public class Spieler {
     //Todo Test this
     public HashSet<Land> getFeinde() {
         HashSet<Land> feinde = new HashSet<>();
-        for (Land kolonie : besetzteLaender){
+        for (Land kolonie : besetzteLaender) {
             feinde.addAll(kolonie.getFeindlicheNachbarn());
         }
         return feinde;
@@ -57,7 +64,7 @@ public class Spieler {
         einheiten += soldaten; //ToDo checke ob es mit Kampf kompatibel ist soldaten mit land.strength zu ersetzen
     }
 
-    public void verliereLand(Land land){
+    public void verliereLand(Land land) {
         einheiten -= land.getEinheiten(); //Kann Probleme machen, falls Einheitenzahl schon mit Gegner ersetzt wurde
         besetzteLaender.remove(land);
     }
@@ -74,7 +81,7 @@ public class Spieler {
         }
         //Zuschuss Kontinente
         Kontinent[] reiche = (Kontinent[]) alleKontinente.stream().filter(kontinent -> kontinent.getEinzigerBesitzer() == this).toArray();
-        if (reiche.length > 0){
+        if (reiche.length > 0) {
             neueEinheiten += Arrays.stream(reiche).mapToInt(Kontinent::getBuff).sum();
         }
 
@@ -83,23 +90,24 @@ public class Spieler {
 
     /**
      * Eine Methode die neue Truppen ihre Länder zuweist
+     *
      * @param truppen Anzahl Truppen die stationiert werden
      */
-    public void zuweisungEinheiten(int truppen){
-        for (int t = 1; t <= truppen; t++){
+    public void zuweisungEinheiten(int truppen) {
+        for (int t = 1; t <= truppen; t++) {
             //ToDO choose an owned country to put the unit in. > add 1 to einheiten here and in chosen land
         }
     }
 
-    public void bewegeEinheiten(int truppen, Land herkunft, Land ziel){ //ToDo Wieso gibt es moveTroops sowohl in Spieler als auch in Spiel?
+    public void bewegeEinheiten(int truppen, Land herkunft, Land ziel) { //ToDo Wieso gibt es moveTroops sowohl in Spieler als auch in Spiel?
         //ToDO throw error
-        if(herkunft.getBesitzer() != this) {
+        if (herkunft.getBesitzer() != this) {
             //throw error (Diese Truppen gehören dir nicht!)
         }
-        if(herkunft.getEinheiten() <= truppen) {
+        if (herkunft.getEinheiten() <= truppen) {
             //throw error (Es befinden sich zu wenig Einheiten auf diesem Feld!)
         }
-        if(!herkunft.connectionPossible(ziel)) {
+        if (!herkunft.connectionPossible(ziel)) {
             //throw error (Die Länder sind nicht verbunden!)
         }
         int herkunftEinheiten = herkunft.getEinheiten();
@@ -110,13 +118,13 @@ public class Spieler {
     //endregion
 
     public void sterben() {
-            this.alive = false;
+        this.alive = false;
     }
 
     //ToDO implement everytime Einheiten get changed
-    public void zaehleEinheiten(){
+    public void zaehleEinheiten() {
         int soldaten = 0;
-        for (Land land : besetzteLaender){
+        for (Land land : besetzteLaender) {
 
             soldaten += land.getEinheiten();
         }
@@ -125,7 +133,7 @@ public class Spieler {
     }
 
     @Override
-    public boolean equals(Object spieler){
+    public boolean equals(Object spieler) {
         return (spieler instanceof Spieler) && ((Spieler) spieler).id == this.id;
     }
 }
