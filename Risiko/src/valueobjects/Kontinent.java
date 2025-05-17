684d7ec727d4fb3a282382306b20bc1e194e83c5
Majbritt Siebenthaler <msiebenthaler@stud.hs-bremen.de>
2025-05-17 10:30:37 +0200

    Kontinent besitzer entfernt und mit getter mit checkBesitzer-Code ersetzt Ansonsten unverändert

--- Risiko/src/valueobjects/Kontinent.java
+++ Risiko/src/valueobjects/Kontinent.java
@@ -1,38 +1,27 @@
+package valueobjects;
+
 public class Kontinent {
     private final String name;
     private final Land[] gebiete;
     private final int buff;
-    private Spieler einzigerBesitzer; //Kann null sein, falls Kontinent mehrere Besatzer hat
 
     public Kontinent(String name, Land[] gebiete, int buff) {
         this.name = name;
         this.gebiete = gebiete;
         this.buff = buff;
-        einzigerBesitzer = null;
     }
+
+    //region getter
     public Land[] getGebiete() {
         return gebiete;
     }
     public int getBuff() {
         return buff;
     }
-    public Spieler getEinzigerBesitzer() {
-        return einzigerBesitzer;
-    }
     public String getName() {
         return name;
     }
-
-    public boolean beinhaltetLand(Land land){
-        for (Land landK : gebiete){
-            if (landK.equals(land)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void checkBesitzer(){
+    public Spieler getEinzigerBesitzer() throws NullPointerException{ //Nicht sicher ob es das ist da es nicht fälschlich versucht auf null zuzugreifen, aber es gibt null zurück und damit sind alle Aufrufe hiervon von Nullpointerexceptions bedroht
         boolean isSame = true;
         for (int i = 1; i < gebiete.length; i++){
             if (!gebiete[i].getBesitzer().equals(gebiete[0].getBesitzer())) {
@@ -41,9 +30,21 @@ public class Kontinent {
             }
         }
         if (isSame){
-            einzigerBesitzer = gebiete[0].getBesitzer();
+            return gebiete[0].getBesitzer();
         } else {
-            einzigerBesitzer = null;
+            return null;
         }
     }
+    //endregion
+
+    //region setter
+    public boolean beinhaltetLand(Land land){
+        for (Land landK : gebiete){
+            if (landK.equals(land)){
+                return true;
+            }
+        }
+        return false;
+    }
+    //endregion
 }
