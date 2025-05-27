a38821089f16ebf3396adc6c3e0f043d55a057e0
GLBMoeller <lenmoeller@stud.hs-bremen.de>
2025-05-27 14:01:18 +0200

    neue Missionsunterklasse erstellt (Länder+Einheiten)

---
+++ Risiko/src/missionen/Laendereroberungplus.java
@@ -0,0 +1,24 @@
+package missionen;
+
+import domain.Spiel;
+import valueobjects.Spieler;
+
+public class Laendereroberungplus extends Laendereroberung {
+    private final int zielAnzahl;
+
+    public Laendereroberungplus(int zielAnzahl) {
+        super(Integer.parseInt("Erobere " + zielAnzahl + " Länder und setzen Sie in jedes Land mindestens 2 Armeen!"));
+        this.zielAnzahl = zielAnzahl;
+    }
+    @Override
+    public boolean istErfuellt(Spiel spiel) {
+        Spieler spieler = spiel.getAktuellerSpieler();
+        int counter = 0;
+        for(int i = 0; spieler.getBesetzteLaender().size()<2; i++) {
+            if(spieler.getBesetzteLaender().get(i).getEinheiten()>=2) {
+                counter++;
+            }
+        }
+        return counter>=zielAnzahl;
+    }
+}
